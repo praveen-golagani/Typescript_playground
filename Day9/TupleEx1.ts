e109d3ffd8f2060ad5180a32cf74ec6c953992cb
praveen-golagani <praveenkqa800@gmail.com>
2025-11-07 22:35:05 +0530

    tuple

--- Day9/TupleEx1.ts
+++ Day9/TupleEx1.ts
@@ -13,3 +13,23 @@ for (let val of bike){
 for (let i in bike){
     console.log(bike[i]);
 }
+// tuple ex2 - tuple of tuples
+let choco:[number,string][] = [[4,'ant'],[66,'man'],[100,'marks']];
+console.log(choco[1]);
+console.log(choco[0][1]);
+console.log(choco[2]);
+console.log(choco[1][0]);
+console.log('---');
+for (let i=0;i<choco.length;i++){
+        for(let j=0;j<choco[i].length;j++){
+            console.log(choco[i][j]);
+        }
+}
+console.log('---');
+//tuple ex3
+let bikes:[number,string,string][] = [[4,'ant','honda'],[66,'man','suzuki'],[100,'marks','ola']];
+for (let i=0;i<bikes.length;i++){
+        for(let j=0;j<bikes[i].length;j++){
+            console.log(bikes[i][j]);
+        }
+}
