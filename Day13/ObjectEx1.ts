4d9cfd00bc3f91f855875e494719b2bd0e8e1197
praveen-golagani <praveenkqa800@gmail.com>
2025-11-09 01:47:55 +0530

    object and class concept

--- Day13/ObjectEx1.ts
+++ Day13/ObjectEx1.ts
@@ -1,16 +1,30 @@
-//key, value pair
-
-
+//key, value pair [properites and behaviour]
+//objext - only meth
 let customer = {
     firstName: 'Praveen',
-    app: 'web Application'
+    app: 'web Application',
+    country: 'India'
 };
-console.log(customer.app);
-console.log(`customer name - ${customer.firstName}, uses : ${customer.app}`);
+// console.log(customer.app);
+// console.log(`customer name - ${customer.firstName}, uses : ${customer.app}`);
+
+// console.log(customer['app']);
+// console.log(customer['country']);
+// //modify
+// customer.app='vs code';
+// console.log(customer.app);
 
 export class CustomerDetails{
 
      printCustomerName():void {
         console.log(customer.firstName);
     }
+
+    printDept(dept:string):void{
+        console.log(`works in ${dept}`)
+    }
+
 };
+
+ export const custObj = new CustomerDetails();
+
---
+++ Day15/Testimpex1.ts
@@ -0,0 +1,8 @@
+//import { CustomerDetails } from "../Day13/ObjectEx1";
+import { custObj } from "../Day13/ObjectEx1";
+console.log('----')
+//let custObj = new CustomerDetails();
+console.log('----')
+custObj.printCustomerName();
+
+custObj.printDept('quality assurance');
