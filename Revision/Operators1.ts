c00e1c7db4a8d739202e83d531a763d4f0bd1378
praveen-golagani <praveenkqa800@gmail.com>
2025-12-25 11:57:34 +0530

    operators parc

--- Revision/Operators1.ts
+++ Revision/Operators1.ts
@@ -1,8 +1,21 @@
-let input1:number = 10;
-let input2:string = '10';
-console.log(input1==input2);
-console.log(input1===input2);
-console.log(input1!=input2);
-console.log(input1!==input2);
+let opInput1:number = 10;
+let opInput2:string = '10';
+// console.log(opInput1==opInput2);
+// console.log(opInput1===opInput2);
+// console.log(opInput1!=opInput2);
+// console.log(opInput1!==opInput2);
+console.log(opInput1**2);
+let operInp3:number = 45;
+operInp3+=2;
+console.log(operInp3);
+console.log(++operInp3);
+console.log(operInp3++);
+//console.log(typeof(opInput1)&&typeof(opInput2));
+console.log(opInput1>5 && operInp3>0);
+console.log(opInput1<5 && operInp3>0);
+console.log(opInput1>5 || operInp3>0);
 
-//
+//ternary
+
+let max:number = opInput1>operInp3?opInput1:operInp3;
+console.log(`${max} is the greater.`);
